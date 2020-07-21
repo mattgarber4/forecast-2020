3e0dc50a792800faea32703e21e55ed5004d4670
Matt Garber <mattgarber4@gmail.com>
2020-07-21 22:50:59 +0000

    add master shell script

--- ec2/forecast-master.sh
+++ ec2/forecast-master.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script to invoke all forecast ec2 processes
 
-cd /home/rstudio/forecast/ec2/
-/usr/lib/R/bin/Rscript 'fb_db_update.R' >> 'fb_db_update.log' 2>&1
+cd /home/rstudio/forecast/
+/usr/lib/R/bin/Rscript 'ec2/fb_db_update.R' >> 'ec2/fb_db_update.log' 2>&1
 
 /usr/local/aws-cli/v2/current/bin/aws lambda invoke --function-name stop-ec2 response.json
