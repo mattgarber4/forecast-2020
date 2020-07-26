ef3ad920e86aa9a45fb03e839f51c8ecbaedf7e5
Matt Garber <mattgarber4@gmail.com>
2020-07-26 18:37:38 +0000

    move drivers for predictit

--- ec2/forecast-master.sh
+++ ec2/forecast-master.sh
@@ -3,3 +3,8 @@
 
 ## facebook data update
 /usr/lib/R/bin/Rscript './ec2/fb_db_update.R' >> './ec2/fb_db_update.log' 2>&1
+
+## run predictit scripts
+/usr/local/aws-cli/v2/current/bin/aws lambda invoke --function-name scrape_predictit response.json
+/usr/local/aws-cli/v2/current/bin/aws lambda invoke --function-name scrape_predictit2 response.json
+/usr/local/aws-cli/v2/current/bin/aws lambda invoke --function-name scrape_predictit3 response.json
