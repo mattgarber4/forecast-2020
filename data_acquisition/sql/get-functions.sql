5c44c2a70944f4cbd96f7cdc7ec0e81a6f3adec3
Matt Garber <48842179+mattgarber4@users.noreply.github.com>
2020-07-26 14:50:29 -0400

    Update get functions to group by days ensures one price per day

--- data_acquisition/sql/get-functions.sql
+++ data_acquisition/sql/get-functions.sql
@@ -1,31 +1,40 @@
+DROP FUNCTION predictit.getdays(date, date);
 CREATE OR REPLACE FUNCTION predictit.getDays(date, date) 
-RETURNS TABLE(state text, date date, dem int, rep int)
-  AS $$ SELECT state, date, dem, rep FROM (
-	  		SELECT state, time::date as date, price as dem
-  			FROM predictit.prices 
-			WHERE time::date >= $1 and time::date <= $2 AND party = 'D') a
-		
-			LEFT JOIN (		
-			SELECT state, time::date as date, price as rep
-  			FROM predictit.prices 
-			WHERE time::date >= $1 and time::date <= $2 AND party = 'R'
-			) b
-			USING(state, date)
-			ORDER BY date asc
+RETURNS TABLE(state text, date date, dem float, rep float)
+  AS $$ SELECT state, date, avg(dem::float) as dem, avg(rep::float) as rep
+	  		FROM (
+				(
+					SELECT state, time::date as date, price as dem
+					FROM predictit.prices 
+					WHERE time::date >= $1 and time::date <= $2 AND party = 'D'
+				) a
+	  			LEFT JOIN (		
+					SELECT state, time::date as date, price as rep
+					FROM predictit.prices 
+					WHERE time::date >= $1 and time::date <= $2 AND party = 'R'
+				) b
+	  			USING(state, date)
+  			)
+	  		GROUP BY state, date
+	  		ORDER BY date asc
 	 $$ LANGUAGE sql;
 
+DROP FUNCTION fb.getdays(date, date);
 CREATE OR REPLACE FUNCTION fb.getDays(date, date) 
-RETURNS TABLE(state text, date date, dem int, rep int)
-	AS $$ SELECT state, date, dem, rep FROM (
-	  		SELECT state, date, amt::int as dem
-  			FROM fb.spend 
-			WHERE date >= $1 and date <= $2 AND party = 'D') a
-		
+RETURNS TABLE(state text, date date, dem float, rep float)
+	AS $$ SELECT state, date, avg(dem::float) as dem, avg(rep::float) as rep FROM (
+			(
+				SELECT state, date, amt::int as dem
+				FROM fb.spend 
+				WHERE date >= $1 and date <= $2 AND party = 'D'
+			) a
 			LEFT JOIN (		
-			SELECT state, date, amt::int as rep
-  			FROM fb.spend 
-			WHERE date >= $1 and date <= $2 AND party = 'R'
+				SELECT state, date, amt::int as rep
+				FROM fb.spend 
+				WHERE date >= $1 and date <= $2 AND party = 'R'
 			) b
 			USING(state, date)
-			ORDER BY date asc
-	 $$ LANGUAGE sql;
+		)
+		GROUP BY date, state
+		ORDER BY date asc
+	$$ LANGUAGE sql;
